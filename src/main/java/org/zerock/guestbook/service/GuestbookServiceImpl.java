dc816a55c47ae65874020821c3b273062433f9d6
WonSik36 <cws0173@gmail.com>
2021-01-19 22:46:49 +0900

    Make list guestbook function #1

---
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -0,0 +1,39 @@
+package org.zerock.guestbook.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.zerock.guestbook.dto.GuestbookDto;
+import org.zerock.guestbook.dto.PageRequestDto;
+import org.zerock.guestbook.dto.PageResultDto;
+import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookServiceImpl implements GuestbookService{
+    private final GuestbookRepository repository;
+
+    @Override
+    public Long register(GuestbookDto dto) {
+        Guestbook entity = repository.save(this.dtoToEntity(dto));
+
+        return entity.getGno();
+    }
+
+    @Override
+    public PageResultDto<GuestbookDto, Guestbook> getList(PageRequestDto requestDto) {
+        Pageable pageable = requestDto.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        return new PageResultDto<>(result, this::entityToDto);
+    }
+}
